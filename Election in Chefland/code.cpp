8b000a11bbff068fb991f1ac6448a6eb9fc4f566
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-20 09:27:05 +0530

    Create code.cpp

---
+++ Election in Chefland/code.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        vector<int> v(3);
+        for (auto &i : v)
+            cin >> i;
+        bool nota = true;
+        int index = 0;
+        int max = v[0];
+        for (int i = 1; i < 3; i++)
+        {
+            if (v[index] < v[i])
+            {
+                index= i; 
+                max = v[i]; 
+            }
+        }
+        if (max > 50)
+        {
+            switch (index)
+            {
+                case 0:
+                    cout << "A" << endl; 
+                    break; 
+                case 1:
+                    cout << "B" << endl; 
+                    break; 
+                case 2:
+                    cout << "C" << endl; 
+                    break;
+            }
+        }
+        else 
+            cout << "NOTA" << endl; 
+    }
+}
